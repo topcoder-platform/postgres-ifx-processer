7f5cbc834db39aed79b52cb91ed6fbd03cd15d4f
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2019-12-18 14:34:31 +0530

    [skip ci]
    [skip ci]

--- src/services/pushToDynamoDb.js
+++ src/services/pushToDynamoDb.js
@@ -18,7 +18,7 @@ async function pushToDynamoDb(payload) {
 		timestamp: Date.now()
     		}
   		}
-	  var docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+	  var docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1',convertEmptyValues: true});
 	  docClient.put(params, function(err, data) {
     	if (err) logger.error(err);
     	else logger.info(data);
