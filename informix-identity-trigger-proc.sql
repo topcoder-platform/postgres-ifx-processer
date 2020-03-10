5c9feecfaa6a75fbc3361583375394715bac11aa
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2020-03-10 15:25:06 +0530

    [skip ci]
    [skip ci]

--- informix-identity-trigger-proc.sql
+++ informix-identity-trigger-proc.sql
@@ -1,4 +1,66 @@
 --databse common_oltp
+DROP PROCEDURE proc_user_update(varchar,decimal);
+DROP PROCEDURE proc_user_update;
+CREATE PROCEDURE informix.proc_user_update(
+new_handle varchar(50),
+user_id decimal(10,0))
+   if (USER != 'ifxsyncuser') then
+UPDATE user SET handle_lower = lower(new_handle), modify_date = current WHERE user.user_id = user_id;
+End if;
+end procedure;                                  
+create procedure "informix".proc_user_update(
+user_id DECIMAL(10,0),
+old_first_name VARCHAR(64),
+new_first_name VARCHAR(64),
+old_last_name VARCHAR(64),
+new_last_name VARCHAR(64),
+old_handle VARCHAR(50),
+new_handle VARCHAR(50),
+old_status VARCHAR(3),
+new_status VARCHAR(3),
+old_activation_code VARCHAR(32),
+new_activation_code VARCHAR(32),
+old_middle_name VARCHAR(64),
+new_middle_name VARCHAR(64),
+old_timezone_id decimal(5,0),
+new_timezone_id decimal(5,0)
+)
+ 
+      if ((old_first_name != new_first_name) or (old_last_name != new_last_name ) or (old_middle_name != new_middle_name )) then
+         insert into audit_user (column_name, old_value, new_value,
+user_id)
+         values ('NAME', NVL(old_first_name, '') || ' ' || NVL(old_middle_name, '') || ' ' || NVL(old_last_name, ''),
+                 NVL(new_first_name, '') || ' ' || NVL(new_middle_name,
+'') || ' ' || NVL(new_last_name, ''), user_id);
+      End if;
+      
+      if (old_handle != new_handle) then 
+         insert into audit_user (column_name, old_value, new_value,
+user_id)
+         values ('HANDLE', old_handle, new_handle, user_id);
+      End If;
+
+      if (old_status != new_status) then 
+         insert into audit_user (column_name, old_value, new_value,
+user_id)
+         values ('STATUS', old_status, new_status, user_id);
+      End If;
+
+      if (old_activation_code != new_activation_code) then 
+         insert into audit_user (column_name, old_value, new_value,
+user_id)
+         values ('ACTIVATION_CODE', old_activation_code, new_activation_code, user_id);
+      End If;
+
+      if (old_timezone_id != new_timezone_id) then 
+         insert into audit_user (column_name, old_value, new_value,
+user_id)
+         values ('TIMEZONE_ID', old_timezone_id, new_timezone_id, user_id);
+      End If;
+      if (USER != 'ifxsyncuser') then
+      UPDATE user SET handle_lower = lower(new_handle), modify_date = current WHERE user.user_id = user_id;
+      End if;
+end procedure;  
 
 DROP PROCEDURE informix.proc_email_update;
 CREATE PROCEDURE informix.proc_email_update(
