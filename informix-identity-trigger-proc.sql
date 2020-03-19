6b64469f571f849835b79ec63fa09181fde42983
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2020-03-19 17:53:41 +0530

    [skip ci]
    [skip ci]

--- informix-identity-trigger-proc.sql
+++ informix-identity-trigger-proc.sql
@@ -1,4 +1,4 @@
---databse common_oltp
+database common_oltp
 DROP PROCEDURE proc_user_update(varchar,decimal);
 DROP PROCEDURE proc_user_update;
 CREATE PROCEDURE informix.proc_user_update(
@@ -101,7 +101,7 @@ new_status_id DECIMAL(3,0)
 end procedure;
 
 
---database informixoltp
+database informixoltp
 DROP PROCEDURE informix.proc_coder_update;
 CREATE PROCEDURE informix.proc_coder_update(
 v_oldcoder_id decimal(10,0),
@@ -135,4 +135,70 @@ v_oldcomp_country_code varchar(3), v_newcomp_country_code varchar(3)
       update coder set modify_date = current where coder_id = v_oldcoder_id;
       End if;
 
+end procedure;
+
+database tcs_catalog;
+DROP PROCEDURE proc_reliability_update;
+CREATE PROCEDURE informix.proc_reliability_update(
+p_user_id DECIMAL(10,0),
+p_phase_id decimal(3,0),
+old_rating decimal(5,4),
+new_rating decimal(5,4)
+)
+
+ if (USER != 'ifxsyncuser') then
+      if (old_rating != new_rating) then 
+         insert into user_reliability_audit (column_name, old_value, new_value, user_id, phase_id)
+         values ('RATING', old_rating, new_rating, p_user_id, p_phase_id);
+      End If;
+
+      update user_reliability set modify_date = current where user_id = p_user_id and phase_id = p_phase_id;
+End if;
+end procedure;   
+
+DROP PROCEDURE proc_rating_update;
+CREATE PROCEDURE informix.proc_rating_update(
+p_user_id DECIMAL(10,0),
+p_phase_id decimal(3,0),
+old_rating decimal(10,0),
+new_rating decimal(10,0),
+old_vol decimal(10,0),
+new_vol decimal(10,0),
+old_num_ratings decimal(5,0),
+new_num_ratings decimal(5,0),
+old_last_rated_project_id decimal(12,0),
+new_last_rated_project_id decimal(12,0)
+)
+ if (USER != 'ifxsyncuser') then
+      if (old_rating != new_rating) then 
+         insert into user_rating_audit (column_name, old_value, new_value, user_id, phase_id)
+         values ('RATING', old_rating, new_rating, p_user_id, p_phase_id);
+      End If;
+
+      if (old_vol != new_vol) then 
+         insert into user_rating_audit (column_name, old_value, new_value, user_id, phase_id)
+         values ('VOL', old_vol, new_vol, p_user_id, p_phase_id);
+      End If;
+
+      if (old_num_ratings != new_num_ratings) then 
+         insert into user_rating_audit (column_name, old_value, new_value, user_id, phase_id)
+         values ('NUM_RATINGS', old_num_ratings, new_num_ratings, p_user_id, p_phase_id);
+      End If;
+
+      if (old_last_rated_project_id != new_last_rated_project_id) then 
+         insert into user_rating_audit (column_name, old_value, new_value, user_id, phase_id)
+         values ('LAST_RATED_PROJECT_ID', old_last_rated_project_id, new_last_rated_project_id, p_user_id, p_phase_id);
+      End If;
+
+      update user_rating set mod_date_time = current where user_id = p_user_id and phase_id = p_phase_id;
+End if;
+end procedure;      
+
+DROP PROCEDURE proc_contest_creation_update;
+CREATE PROCEDURE informix.proc_contest_creation_update (create_user VARCHAR(64), old_project_status_id INT, new_project_status_id INT)
+if (USER != 'ifxsyncuser') then
+      if (old_project_status_id != new_project_status_id and new_project_status_id == 1) then
+         insert into corona_event (corona_event_type_id,user_id)  values (5, TO_NUMBER(create_user));
+      end if;
+End if;
 end procedure;
