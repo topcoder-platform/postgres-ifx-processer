d0d9e8e92eb96ebb160fe4f7440a1a25f86662c3
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2020-05-22 17:23:56 +0530

    bit clean-up

--- src/consumer.js
+++ src/consumer.js
@@ -22,16 +22,6 @@ const options = {
 };
 const consumer = new Kafka.GroupConsumer(options);
 
-/*const consumer = new Kafka.SimpleConsumer({
-  connectionString: kafkaOptions.brokers_url,
-  ...(isSslEnabled && { // Include ssl options if present
-    ssl: {
-      cert: kafkaOptions.SSL.cert,
-      key: kafkaOptions.SSL.key
-    }
-  })
-})*/
-
 const check = function () {
   if (!consumer.client.initialBrokers && !consumer.client.initialBrokers.length) {
     return false;
@@ -46,16 +36,17 @@ const check = function () {
 
 let cs_processId;
 const terminate = () => process.exit()
+
 /**
  *
  * @param {Array} messageSet List of messages from kafka
  * @param {String} topic The name of the message topic
  * @param {Number} partition The kafka partition to which messages are written
  */
-var retryvar="";
+var retryvar = "";
 //let cs_payloadseqid;
 async function dataHandler(messageSet, topic, partition) {
-        for (const m of messageSet) { // Process messages sequentially
+  for (const m of messageSet) { // Process messages sequentially
     let message
     try {
       let ifxstatus = 0
@@ -64,39 +55,39 @@ async function dataHandler(messageSet, topic, partition) {
       //logger.debug(`Consumer Received from kafka :${JSON.stringify(message)}`)
       if (message.payload.payloadseqid) cs_payloadseqid = message.payload.payloadseqid;
       logger.debug(`consumer : ${message.payload.payloadseqid} ${message.payload.table} ${message.payload.Uniquecolumn} ${message.payload.operation} ${message.timestamp} `);
-       //await updateInformix(message)
-       ifxstatus = await updateInformix(message)
-       if (ifxstatus === 0 && `${message.payload.operation}` === 'INSERT') {
+      //await updateInformix(message)
+      ifxstatus = await updateInformix(message)
+      if (ifxstatus === 0 && `${message.payload.operation}` === 'INSERT') {
         logger.debug(`Consumer :informixt status for ${message.payload.table} ${message.payload.payloadseqid} : ${ifxstatus} - Retrying`)
 
-       auditTrail([cs_payloadseqid,cs_processId,message.payload.table,message.payload.Uniquecolumn,
-            message.payload.operation,"push-to-kafka",retryvar,"","",JSON.stringify(message), new Date(),message.topic],'consumer')
-         await retrypushtokakfa(message,topic,m,partition)
-      }
-      else {
+        auditTrail([cs_payloadseqid, cs_processId, message.payload.table, message.payload.Uniquecolumn,
+          message.payload.operation, "push-to-kafka", retryvar, "", "", JSON.stringify(message), new Date(), message.topic], 'consumer')
+        await retrypushtokakfa(message, topic, m, partition)
+      } else {
         if (message.payload['retryCount']) retryvar = message.payload.retryCount;
-       auditTrail([cs_payloadseqid,cs_processId,message.payload.table,message.payload.Uniquecolumn,
-       message.payload.operation,"Informix-updated",retryvar,"","",JSON.stringify(message), new Date(),message.topic],'consumer')
-      logger.debug(`Consumer :informix status for ${message.payload.table} ${message.payload.payloadseqid} : ${ifxstatus}`)
-      await consumer.commitOffset({ topic, partition, offset: m.offset }) // Commit offset only on success
-    } }catch (err) {
+        auditTrail([cs_payloadseqid, cs_processId, message.payload.table, message.payload.Uniquecolumn,
+          message.payload.operation, "Informix-updated", retryvar, "", "", JSON.stringify(message), new Date(), message.topic], 'consumer')
+        logger.debug(`Consumer :informix status for ${message.payload.table} ${message.payload.payloadseqid} : ${ifxstatus}`)
+      }
+    } catch (err) {
       const errmsg2 = `error-sync: Could not process kafka message or informix DB error: "${err.message}"`
       logger.error(errmsg2)
       logger.debug(`error-sync: consumer "${err.message}"`)
-      await retrypushtokakfa(message,topic,m,partition)
+      await retrypushtokakfa(message, topic, m, partition)
+    } finally {
+      await consumer.commitOffset({ topic, partition, offset: m.offset }) // Commit offset only on success
     }
   }
 }
 
-async function retrypushtokakfa(message,topic,m,partition)
-{
-let cs_payloadseqid
-logger.debug(`Consumer : At retry function`)
-       if (!cs_payloadseqid){
-            cs_payloadseqid= 'err-'+(new Date()).getTime().toString(36) + Math.random().toString(36).slice(2);}
+async function retrypushtokakfa(message, topic, m, partition) {
+  let cs_payloadseqid
+  logger.debug(`Consumer : At retry function`)
+  if (!cs_payloadseqid) {
+    cs_payloadseqid = 'err-' + (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2);
+  }
   try {
     if (message.payload['retryCount']) retryvar = message.payload.retryCount;
-    await consumer.commitOffset({ topic, partition, offset: m.offset }) // Commit success as will re-publish
     logger.debug(`Trying to push same message after adding retryCounter`)
     if (!message.payload.retryCount) {
       message.payload.retryCount = 0
@@ -120,34 +111,36 @@ logger.debug(`Consumer : At retry function`)
   }
   catch (err) {
     await auditTrail([cs_payloadseqid, cs_processId, message.payload.table, message.payload.Uniquecolumn,
-    message.payload.operation, "Error-republishing", message.payload['retryCount'], err.message, "", message.payload.data, new Date(), message.topic], 'consumer')
+      message.payload.operation, "Error-republishing", message.payload['retryCount'], err.message, "", message.payload.data, new Date(), message.topic], 'consumer')
     const errmsg1 = `error-sync: postgres-ifx-processor: consumer : Error-republishing: "${err.message}"`
     logger.error(errmsg1)
     logger.debug(`error-sync: consumer re-publishing "${err.message}"`)
-   // await callposttoslack(errmsg1)
+    // await callposttoslack(errmsg1)
+  } finally {
+    await consumer.commitOffset({ topic, partition, offset: m.offset }) // Commit success as will re-publish
   }
 }
 
 async function callposttoslack(slackmessage) {
-if(config.SLACK.SLACKNOTIFY === 'true') {
+  if (config.SLACK.SLACKNOTIFY === 'true') {
     return new Promise(function (resolve, reject) {
-        postMessage(slackmessage, (response) => {
-          console.log(`respnse : ${response}`)
-                if (response.statusCode < 400) {
-                logger.debug('Message posted successfully');
-                //callback(null);
-            } else if (response.statusCode < 500) {
-                const errmsg1 =`Slack Error: posting message to Slack API: ${response.statusCode} - ${response.statusMessage}`
-                logger.debug(`error-sync: ${errmsg1}`)
-            }
-                else {
-                logger.debug(`Server error when processing message: ${response.statusCode} - ${response.statusMessage}`);
-                //callback(`Server error when processing message: ${response.statusCode} - ${response.statusMessage}`);
-            }
-                resolve("done")
-        });
+      postMessage(slackmessage, (response) => {
+        console.log(`respnse : ${response}`)
+        if (response.statusCode < 400) {
+          logger.debug('Message posted successfully');
+          //callback(null);
+        } else if (response.statusCode < 500) {
+          const errmsg1 = `Slack Error: posting message to Slack API: ${response.statusCode} - ${response.statusMessage}`
+          logger.debug(`error-sync: ${errmsg1}`)
+        }
+        else {
+          logger.debug(`Server error when processing message: ${response.statusCode} - ${response.statusMessage}`);
+          //callback(`Server error when processing message: ${response.statusCode} - ${response.statusMessage}`);
+        }
+        resolve("done")
+      });
     }) //end
-}
+  }
 
 }
 
@@ -156,20 +149,17 @@ if(config.SLACK.SLACKNOTIFY === 'true') {
  */
 async function setupKafkaConsumer() {
   try {
-   const strategies = [{
+    const strategies = [{
       subscriptions: [kafkaOptions.topic],
       handler: dataHandler
     }];
     await consumer.init(strategies)
-    //await consumer.subscribe(kafkaOptions.topic, kafkaOptions.partition, { time: Kafka.LATEST_OFFSET }, dataHandler)
-    //await consumer.subscribe(kafkaOptions.topic, dataHandler)
-	  
     logger.info('Initialized kafka consumer')
     healthcheck.init([check])
   } catch (err) {
     logger.error('Could not setup kafka consumer')
     logger.logFullError(err)
-    logger.debug(`error-sync: consumer kafka-setup "${err.message}"`) 
+    logger.debug(`error-sync: consumer kafka-setup "${err.message}"`)
     terminate()
   }
 }
--- src/services/updateInformix.js
+++ src/services/updateInformix.js
@@ -2,19 +2,18 @@
 const informix = require('../common/informixWrapper')
 const logger = require('../common/logger')
 
-String.prototype.escapeSpecialChars = function() {
-        return this.replace(/\n/g, "\\n");
+String.prototype.escapeSpecialChars = function () {
+  return this.replace(/\n/g, "\\n");
 };
 
-async function updateInformix (payload) {
+async function updateInformix(payload) {
   logger.debug(`Informix Received from consumer-kafka :${JSON.stringify(payload)}`)
   const operation = payload.payload.operation.toLowerCase()
-  console.log("=====Informix DML Operation :==========",operation)
-	let sql = null
-	let t0 = []
-	let paramvalue = null
-        const columns = payload.payload.data
-        const primaryKey = payload.payload.Uniquecolumn
+  console.log("=====Informix DML Operation :==========", operation)
+  let sql = null
+  let t0 = []
+  const columns = payload.payload.data
+  const primaryKey = payload.payload.Uniquecolumn
   // Build SQL query
   switch (operation) {
     case 'insert':
@@ -26,15 +25,15 @@ async function updateInformix (payload) {
       break
     case 'update':
       {
-	  sql = `update ${payload.payload.schema}:${payload.payload.table} set ${Object.keys(columns).map((key) => `${key}= ?`).join(', ')} where ${primaryKey}= ?;`
-	  t0 = Object.keys(columns).map((key) => `{"value":"${columns[key]}"}`)
-          t0.push(`{"value":"${columns[primaryKey]}"}`) //param value for appended for where clause
+        sql = `update ${payload.payload.schema}:${payload.payload.table} set ${Object.keys(columns).map((key) => `${key}= ?`).join(', ')} where ${primaryKey}= ?;`
+        t0 = Object.keys(columns).map((key) => `{"value":"${columns[key]}"}`)
+        t0.push(`{"value":"${columns[primaryKey]}"}`) //param value for appended for where clause
       }
       break
     case 'delete':
       {
-	 sql = `delete from ${payload.payload.schema}:${payload.payload.table} where ${primaryKey}= ?;`
-	 t0.push(`{"value":"${columns[primaryKey]}"}`)
+        sql = `delete from ${payload.payload.schema}:${payload.payload.table} where ${primaryKey}= ?;`
+        t0.push(`{"value":"${columns[primaryKey]}"}`)
       }
       break
     default:
@@ -44,13 +43,13 @@ async function updateInformix (payload) {
   //Preparedstatement for informix
   t0.forEach((name, index) => t0[index] = `${name.escapeSpecialChars()}`);
   logger.debug(`Param values : ${t0}`);
-  var temp1 = "[" + `${t0}` + "]"
-  var finalparam = JSON.parse(temp1)
-  
+  let temp1 = "[" + `${t0}` + "]"
+  let finalparam = JSON.parse(temp1)
+
   /*console.log(`Typeof finalparam : ${typeof(finalparam)}`)
   if (finalparam.constructor === Array ) console.log('isarray')
   else console.log('finalparam not an array')*/
-     
+  logger.debug(`Operation ${operation} : Final sql and param values are - ${sql} ${finalparam}`);
   const result = await informix.executeQuery(payload.payload.schema, sql, finalparam)
   return result
 }
