16b08c33d0914edef2f84bce10456482a8c4aa1b
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2020-06-13 11:34:45 +0530

    [skip ci]

--- src/services/auditTrail.js
+++ src/services/auditTrail.js
@@ -57,6 +57,7 @@ pgpool.on('error', (err, client) => {
       if (err) {
         return logger.debug(`Error executing Query : ${err.stack}`)
       }
+      logger.debug(`Audit Trail Sql : ${sql}`)
       logger.debug(`Audit Trail Params : ${data}`)
       logger.debug(`Audit Trail update : ${res.rowCount}`)
     })
